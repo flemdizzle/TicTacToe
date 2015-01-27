8199598faf16299c1c088bff1a69fcffce9a95a7
flemdizzle <fleminsh@gmail.com>
2015-01-26 19:50:09 -0500

    got the Xs and Os popping up where they should be

--- react.jsx
+++ react.jsx
@@ -56,10 +56,10 @@ var Game = React.createClass({
       this.setState(this.getInitialState());
     },
     //Tile click method to modify the state of the tiles array
-    tileClick: function(position, player) {
+    tileClick: function(tileArray, position, player) {
         var tiles = this.state.tiles;
-        if(tiles[position] !== '') return;
-        tiles[position] = player;
+        if(tiles[tileArray][position] !== '') return;
+        tiles[tileArray][position] = player;
         this.setState({tiles: tiles});
         if(!this.checkWinner()){
           this.setState({turn: player === 'O' ? 'X' : 'O'});
@@ -94,7 +94,7 @@ var Game = React.createClass({
 var Tile = React.createClass({
     //The method to handle when a user clicks on the tile, calls the tileClick method on the parent component that is referenced in the props object.
     clickHandler: function() {
-        this.props.tileClick(this.props.position, this.props.turn);
+        this.props.tileClick(this.props.tileArray, this.props.position, this.props.turn);
     },
     render: function() {
         return <div className={this.props.status === '' ? 'tile' : 'tile status-' + this.props.status} onClick={this.clickHandler}>{this.props.status}</div>;
